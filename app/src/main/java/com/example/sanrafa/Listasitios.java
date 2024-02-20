26ff2004a8f7dd70a2821567f2c6b28877f9a38e
Arley <Arley11zx@gmail.com>
2024-02-20 07:42:33 -0500

    Se empieza a crear listas y moldes

---
+++ app/src/main/java/com/example/sanrafa/Listasitios.java
@@ -0,0 +1,14 @@
+package com.example.sanrafa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listasitios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listasitios);
+    }
+}
